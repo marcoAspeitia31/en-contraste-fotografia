318782eba04ea1b3e5a14500369a65b28fab8828
Marco Aspeitia <maspeitia.31@gmail.com>
2023-12-13 22:26:40 -0600

    enqueueing css styles

--- functions.php
+++ functions.php
@@ -138,10 +138,17 @@ add_action( 'widgets_init', 'en_contraste_fotografia_widgets_init' );
  * Enqueue scripts and styles.
  */
 function en_contraste_fotografia_scripts() {
-	wp_enqueue_style( 'en-contraste-fotografia-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '4.5.0' );
+	wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css', array(), '5.8.0' );
+	wp_enqueue_style( 'magnific-popup', get_template_directory_uri() . '/assets/css/magnific-popup.css', array(), EN_CONTRASTE_FOTOGRAFIA_VERSION );
+	wp_enqueue_style( 'animate', get_template_directory_uri() . '/assets/css/animate.min.css', array(), '3.7.2' );
+	wp_enqueue_style( 'slick', get_template_directory_uri() . '/assets/css/slick.css', array(), EN_CONTRASTE_FOTOGRAFIA_VERSION );
+	wp_enqueue_style( 'default', get_template_directory_uri() . '/assets/css/default.css', array(), EN_CONTRASTE_FOTOGRAFIA_VERSION );
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/style.css', array(), EN_CONTRASTE_FOTOGRAFIA_VERSION );
+	wp_enqueue_style( 'en-contraste-fotografia-style', get_stylesheet_uri(), array(), EN_CONTRASTE_FOTOGRAFIA_VERSION );
 	wp_style_add_data( 'en-contraste-fotografia-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'en-contraste-fotografia-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'en-contraste-fotografia-navigation', get_template_directory_uri() . '/js/navigation.js', array(), EN_CONTRASTE_FOTOGRAFIA_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
