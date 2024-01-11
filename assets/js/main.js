9aedc769a8196bb441a1876a023214fe2c0f96c1
Marco Aspeitia <maspeitia.31@gmail.com>
2024-01-10 22:30:04 -0600

    render map if div exists

--- assets/js/main.js
+++ assets/js/main.js
@@ -697,6 +697,59 @@
             }
         });
 
+        if ( $('#map').length ) {
+
+            (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+            key: object.theme_options.google_api_key,
+            v: "weekly",
+            // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+            // Add other bootstrap parameters as needed, using camel case.
+            });
+        
+            // Initialize and add the map
+            let map;
+        
+            async function initMap() {
+    
+                // The location of Uluru
+                const position = { lat: Number( object.theme_options.location.latitude ), lng: Number( object.theme_options.location.longitude ) };
+                // Request needed libraries.
+                //@ts-ignore
+                // Request needed libraries.
+                const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+                const { AdvancedMarkerElement } = await google.maps.importLibrary(
+                    "marker",
+                );
+        
+                // The map, centered at Uluru
+                map = new Map(document.getElementById("map"), {
+                    zoom: 15,
+                    center: position,
+                    mapId: "theme-options-map",
+                });
+        
+                const infoWindow = new InfoWindow();
+        
+                // The marker, positioned at Uluru
+                const marker = new AdvancedMarkerElement({
+                    map: map,
+                    position: position,
+                    title: object.theme_options.business_address,
+                });
+        
+                // Add a click listener for each marker, and set up the info window.
+                marker.addListener('click', ({ domEvent, latLng }) => {
+                    const { target } = domEvent;
+                    infoWindow.close();
+                    infoWindow.setContent(marker.title);
+                    infoWindow.open(marker.map, marker);
+                });
+            }
+    
+            initMap();
+
+        }
+
     });
 
    
@@ -728,41 +781,6 @@
         $('.preloader').delay(500).fadeOut(500);
 
     });
-
-    (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: object.theme_options.google_api_key,
-    v: "weekly",
-    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-    // Add other bootstrap parameters as needed, using camel case.
-    });
-
-    // Initialize and add the map
-    let map;
-
-    async function initMap() {
-        // The location of Uluru
-        const position = { lat: Number( object.theme_options.location.latitude ), lng: Number( object.theme_options.location.longitude ) };
-        // Request needed libraries.
-        //@ts-ignore
-        const { Map } = await google.maps.importLibrary("maps");
-        const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-        // The map, centered at Uluru
-        map = new Map(document.getElementById("map"), {
-            zoom: 15,
-            center: position,
-            mapId: "theme-options-map",
-        });
-
-        // The marker, positioned at Uluru
-        const marker = new AdvancedMarkerElement({
-            map: map,
-            position: position,
-            title: object.theme_options.business_address,
-        });
-    }
-
-    initMap();
         
 
 })( jQuery );
